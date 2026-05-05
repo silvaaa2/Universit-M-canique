4fba021d8672b110233616c4ca7be22f161793da
silvaaa2 <dr947695@gmail.com>
2026-05-05 20:49:00 +0200

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,1 +1,8 @@
+function goPage(url) {
+  loaderText.textContent = "Ouverture de l’espace...";
+  loader.classList.remove("hide");
 
+  setTimeout(() => {
+    window.location.href = url;
+  }, 650);
+}
