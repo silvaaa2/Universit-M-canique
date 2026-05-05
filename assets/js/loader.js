3d8734e4f39fa8001cfaf86b1a826ce296d9f0a2
silvaaa2 <dr947695@gmail.com>
2026-05-05 20:48:41 +0200

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,1 +1,8 @@
+const loader = document.getElementById("loader");
+const loaderText = document.getElementById("loaderText");
 
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.classList.add("hide");
+  }, 1000);
+});
