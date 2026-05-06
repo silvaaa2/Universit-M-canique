4864f19817bd5a545956055d200806ab54a92dfb
silvaaa2 <dr947695@gmail.com>
2026-05-06 17:43:29 +0200

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,3 +1,13 @@
-function goPage(url) {
-  window.location.href = url;
-}
+window.goPage = function (url) {
+  const loader = document.getElementById("loader");
+
+  if (loader) {
+    loader.classList.add("hide");
+    loader.style.opacity = "0";
+    loader.style.visibility = "hidden";
+    loader.style.pointerEvents = "none";
+    loader.style.display = "none";
+  }
+
+  window.location.assign(url);
+};
