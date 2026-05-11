ce0cc1806f94085c62561675da1df468a20a2171
silvaaa2 <dr947695@gmail.com>
2026-05-11 12:33:38 +0200

    Update prof-guard.js

--- assets/js/prof-guard.js
+++ assets/js/prof-guard.js
@@ -43,7 +43,7 @@ onAuthStateChanged(auth, async (user) => {
   }
 
   try {
-    await import("./rp-loader-9kq4z.js?v=5002");
+    await import("./rp-loader-9kq4z.js?v=5003");
   } catch (error) {
     console.error("Erreur chargement loader réponses :", error);
 
