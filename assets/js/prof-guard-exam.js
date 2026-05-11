5e101b5d5158cd96f2bd61496873305a741f7e6e
silvaaa2 <dr947695@gmail.com>
2026-05-11 13:43:57 +0200

    Update prof-guard-exam.js

--- assets/js/prof-guard-exam.js
+++ assets/js/prof-guard-exam.js
@@ -76,7 +76,7 @@ onAuthStateChanged(auth, async (user) => {
   }
 
   try {
-    await import("./exam-loader-x8p2.js?v=7002");
+    await import("./exam-loader-x8p2.js?v=7003");
   } catch (error) {
     console.error("Erreur chargement loader examens :", error);
 
