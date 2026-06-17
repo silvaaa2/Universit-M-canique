c5fd812eb529eec31eeaf0c1b259804ed3881450
silvaaa2 <dr947695@gmail.com>
2026-06-17 12:44:19 +0100

    Load modules table color styles

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -11,3 +11,14 @@ window.goPage = function (url) {
 
   window.location.assign(url);
 };
+
+(function loadModulesTableStyles() {
+  if (!document.body?.classList.contains("modules-page")) return;
+  if (document.querySelector("link[data-modules-table-colors]")) return;
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "../assets/css/prof-modules-eleves.css?v=1001";
+  link.dataset.modulesTableColors = "true";
+  document.head.appendChild(link);
+})();
