9d8c5c72c8ae960e4c5162c65ab694a1daa0d01a
silvaaa2 <dr947695@gmail.com>
2026-07-28 21:49:24 +0100

    Load modules alerts and fresh styling

--- assets/js/prof-modules-eleves.js
+++ assets/js/prof-modules-eleves.js
@@ -1,1 +1,10 @@
+if (!document.querySelector("link[data-modules-table-colors]")) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "../assets/css/prof-modules-eleves.css?v=1003";
+  link.dataset.modulesTableColors = "true";
+  document.head.appendChild(link);
+}
+
 import "./prof-modules-eleves-shared.js?v=1001";
+import "./prof-modules-alerts.js?v=1001";
