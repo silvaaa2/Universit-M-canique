dea7dd5197c750dc89f1009b8c92b9cba3e1f6eb
silvaaa2 <dr947695@gmail.com>
2026-07-28 21:58:14 +0100

    Use safe modules loader

--- assets/js/prof-modules-eleves.js
+++ assets/js/prof-modules-eleves.js
@@ -1,14 +1,14 @@
 const modulesStyle = document.querySelector("link[data-modules-table-colors]");
 
 if (modulesStyle) {
-  modulesStyle.href = "../assets/css/prof-modules-eleves.css?v=1003";
+  modulesStyle.href = "../assets/css/prof-modules-eleves.css?v=1004";
 } else {
   const link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = "../assets/css/prof-modules-eleves.css?v=1003";
+  link.href = "../assets/css/prof-modules-eleves.css?v=1004";
   link.dataset.modulesTableColors = "true";
   document.head.appendChild(link);
 }
 
-import "./prof-modules-eleves-shared.js?v=1001";
+import "./prof-modules-eleves-safe.js?v=1001";
 import "./prof-modules-alerts.js?v=1001";
